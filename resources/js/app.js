8b842c208ef41e824f6baa0fd6ffe39b05161956
alvand soroush <alvand.soroush.2021@gmail.com>
2021-09-05 04:08:36 -0700

    example-vue-2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from "vue";
+
+Vue.component('example' , require('./components/Example').default);
+
+Vue = new Vue({
+    el:"#app",
+    data:{
+        message: "Hello Vue"
+    }
+});
---
+++ resources/js/components/Example.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container text-center">
+    <h3>Hello Seyed Soroush Sarizadeh</h3>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "Example"
+}
+</script>
+
+<style scoped>
+
+</style>
